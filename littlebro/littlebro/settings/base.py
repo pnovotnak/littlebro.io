8069ccd01c05b0945d9c1db309d60791dd27bcc9
Peter Novotnak <pnovotnak@flexion.us>
2014-09-27 22:17:19 -0700

    added static and media roots to base config

--- littlebro/littlebro/settings/base.py
+++ littlebro/littlebro/settings/base.py
@@ -98,9 +98,11 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media files (Uploads)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 try:
